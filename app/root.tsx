a96e002c36c6a3400bf45d24ca6658e57a2a737c
Scai <59282365+alexevladgabriel@users.noreply.github.com>
2025-03-29 02:47:28 +0200

    add home link

--- app/root.tsx
+++ app/root.tsx
@@ -6,10 +6,10 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from 'react-router';
+} from "react-router";
 
-import type { Route } from './+types/root';
-import './app.css';
+import type { Route } from "./+types/root";
+import "./app.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -21,6 +21,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <Link to="/">Home</Link>
         <Link to="/minecraft-server">MC Server</Link>
         <Link to="/cs2-server">CS2 Server</Link>
         {children}
@@ -36,15 +37,15 @@ export default function App() {
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = 'Oops!';
-  let details = 'An unexpected error occurred.';
+  let message = "Oops!";
+  let details = "An unexpected error occurred.";
   let stack: string | undefined;
 
   if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? '404' : 'Error';
+    message = error.status === 404 ? "404" : "Error";
     details =
       error.status === 404
-        ? 'The requested page could not be found.'
+        ? "The requested page could not be found."
         : error.statusText || details;
   } else if (import.meta.env.DEV && error && error instanceof Error) {
     details = error.message;

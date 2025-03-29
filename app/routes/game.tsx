b7f0b51f7322107df021a0883c76f99516e25a68
alexevladgabriel <git@stackblitz.com>
2025-03-29 02:27:46 +0200

    Initial commit

---
+++ app/routes/game.tsx
@@ -0,0 +1,24 @@
+import type { MetaArgs } from 'react-router';
+import type { Route } from './+types/game';
+
+export const gamesData = ['minecraft-server', 'cs2-server'];
+
+export function meta({ data: game }: MetaArgs) {
+  return [
+    {
+      title: `${game} Server Hosting`,
+    },
+  ];
+}
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const current = gamesData.find((game) => game === params.gameId);
+
+  if (!current) {
+    return null;
+  }
+
+  return current;
+}
+
+export function Game({}) {}
---
+++ react-router.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from '@react-router/dev/config';
+import { gamesData } from '~/routes/game';
+
+export default {
+  // Config options...
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: false,
+  async prerender() {
+    return ['/', ...gamesData];
+  },
+} satisfies Config;
